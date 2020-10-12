fabc7bb85a33c22cf9054fa4ddcbe37735a2da27
kevinq22 <70749270+kevinq22@users.noreply.github.com>
2020-10-11 20:51:30 -1000

    Task 6(1)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -57,6 +57,13 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
+   //an array of squares. each square has their own location distinguished by (col,row)
+    const location = [
+        [1,1], [2,1], [3,1],
+        [1,2], [2,2], [3,2],
+        [1,3], [2,3], [3,3]
+    ]
+
     const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -66,7 +73,8 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares
+        squares: squares,
+        location: location[i]
       }
       ]),
       stepNumber:history.length,
@@ -88,7 +96,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-          'Go to move #' + move:
+          'Go to move #' + move + ' (' + history[move].location + ')':
           'Go to game start';
       return (
           <li key={move}>
